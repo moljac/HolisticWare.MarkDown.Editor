89839cce58d86f66c8d4a5934b3d6602a22dfb7e
moljac <mcvjetko@holisticware.net>
2014-10-24 20:14:32 +0200

    gtk# version start

---
+++ assemblies-presentation-ui/HolisticWare.MarkDown.Editor.Presentation.DLL.GTK/WidgetMarkDownEditor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolisticWare.MarkDown.Editor.Presentation.DLL.GTK
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class WidgetMarkDownEditor : Gtk.Bin
+	{
+		public WidgetMarkDownEditor ()
+		{
+			this.Build ();
+		}
+	}
+}
+
